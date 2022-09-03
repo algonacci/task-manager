bf3736bd76055597087f9ac95e085e80ead878bd
algonacci <algonacci1@gmail.com>
2022-09-03 10:55:59 +0700

    🚀 Initial Commit

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log("Task Manager App");
+const express = require("express");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`🚀[SERVER] Listening to http://localhost:${PORT}`);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("Task Manager!");
+});
