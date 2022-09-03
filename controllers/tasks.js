465810e6d160a32b6698740892f9ea5a54710323
algonacci <algonacci1@gmail.com>
2022-09-03 15:32:07 +0700

    Setup schema and controllers logic

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,21 +1,67 @@
-const getAllTasks = (req, res) => {
-  res.send("Get all task!");
+const Task = require("../models/Task");
+
+const getAllTasks = async (req, res) => {
+  try {
+    const tasks = await Task.find({});
+    res.status(200).json({ tasks });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
 };
 
-const createTasks = (req, res) => {
-  res.json(req.body);
+const createTasks = async (req, res) => {
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({
+      message: error,
+    });
+  }
 };
 
-const getTask = (req, res) => {
-  res.json({ id: req.params.id });
+const getTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOne({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `No task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
-const updateTask = (req, res) => {
-  res.send("Task Updated!");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findByIdAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!task) {
+      return res.status(404).json({ message: `No task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
-const deleteTask = (req, res) => {
-  res.send("Task Deleted!");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ message: `No task with ID: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ message: error });
+  }
 };
 
 module.exports = {
--- models/Task.js
+++ models/Task.js
@@ -1,8 +1,16 @@
 const mongoose = require("mongoose");
 
 const TaskSchema = new mongoose.Schema({
-  name: String,
-  completed: Boolean,
+  name: {
+    type: String,
+    required: [true, "Must provide the task name"],
+    trim: true,
+    maxlength: [20, "Task name cannot be more of 20 characters"],
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 module.exports = mongoose.model("Task", TaskSchema);
